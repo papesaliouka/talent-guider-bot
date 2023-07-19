d168a9341c92f991204771c4bafd5390f3d2fffe
papesaliouka <papesaliouka@gmail.com>
2023-07-19 16:48:53 +0000

    removed rtt from ping

--- commands/ping.go
+++ commands/ping.go
@@ -2,7 +2,6 @@ package commands
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -11,20 +10,11 @@ func handlePingInteraction(s *discordgo.Session, i *discordgo.InteractionCreate)
 	// Calculate the API latency
 	apiLatency := s.HeartbeatLatency().Seconds() * 1000
 
-	// Record the start time
-	startTime := time.Now()
-
-	// Send the "Pong!" response
-
-
-	// Calculate the round-trip time (RTT)
-	rtt := time.Since(startTime).Seconds() * 1000
-
 	// Send a follow-up message with latency information
 	response := discordgo.InteractionResponse{
 		Type: discordgo.InteractionResponseChannelMessageWithSource,
 		Data: &discordgo.InteractionResponseData{
-			Content: fmt.Sprintf("API Latency: %.2fms\nRound-trip Time (RTT): %.2fms", apiLatency, rtt),
+			Content: fmt.Sprintf("API Latency: %.2fms", apiLatency),
 		},
 	}
 
