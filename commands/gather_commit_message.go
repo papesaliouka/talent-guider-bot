ef2b21ca388b9ceb3353eaa6c37aef11d8f0dd9e
papesaliouka <papesaliouka@gmail.com>
2023-07-21 15:53:04 +0000

    reviewing names for commit table

--- commands/gather_commit_message.go
+++ commands/gather_commit_message.go
@@ -4,10 +4,19 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"go.mongodb.org/mongo-driver/bson"
 )
+
+type CommitMessage struct {
+	Username    string `json:"username"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Repo        string `json:"repoName"`
+	Branch      string `json:"branchName"`
+}
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Define the channel IDs for commit messages
@@ -26,12 +35,15 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		username := embed.Author.Name
 		description := embed.Description
 		title := embed.Title
+		repo, branch := extractRepoAndBranch(title)
 
 		// Call the saveCommitMessage function to save the data to MongoDB
 		err := saveCommitMessage(CommitMessage{
 			Username:    username,
 			Title:       title,
 			Description: description,
+			Repo:        repo,
+			Branch:      branch,
 		})
 		if err != nil {
 			fmt.Println(err.Error())
@@ -48,10 +60,19 @@ func containsBis(arr []string, str string) bool {
 	return false
 }
 
-type CommitMessage struct {
-	Username    string `json:"username"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
+func extractRepoAndBranch(title string) (repo, branch string) {
+	// Find the position of "[" and "]"
+	openBracket := strings.Index(title, "[")
+	closeBracket := strings.Index(title, "]")
+
+	// Find the position of ":"
+	colon := strings.Index(title, ":")
+
+	// Extract the repoName and branchName using the positions found
+	repo = title[openBracket+1 : colon]
+	branch = title[colon+1 : closeBracket]
+
+	return repo, branch
 }
 
 func saveCommitMessage(commitMsg CommitMessage) error {
@@ -59,9 +80,11 @@ func saveCommitMessage(commitMsg CommitMessage) error {
 
 	// Insert the data into MongoDB
 	_, err := collection.InsertOne(context.Background(), bson.M{
-		"username":    commitMsg.Username,
+		"gitUsername": commitMsg.Username,
 		"title":       commitMsg.Title,
 		"description": commitMsg.Description,
+		"repo":        commitMsg.Repo,
+		"branch":      commitMsg.Branch,
 	})
 	if err != nil {
 		log.Printf("Failed to insert data into MongoDB: %v", err)
